0e4ca6ec8ff5b8638b6bf79a3d03a572ada3e445
negat <negat@microsoft.com>
2017-06-30 23:23:33 +0000

    moved out of /data because this dir doens't exist

--- appdraining-linux-outbound/installonstartstop.sh
+++ appdraining-linux-outbound/installonstartstop.sh
@@ -5,11 +5,11 @@ URLBASE=$1
 echo ${URLBASE} > /pip.txt # save the PIP to curl on stop
 
 curl ${URLBASE}/onstartstop.service > /etc/systemd/system/onstartstop.service
-curl ${URLBASE}/onstart.sh > /data/onstart.sh
-curl ${URLBASE}/onstop.sh > /data/onstop.sh
+curl ${URLBASE}/onstart.sh > /onstart.sh
+curl ${URLBASE}/onstop.sh > /onstop.sh
 
 chmod 755 /etc/systemd/system/onstartstop.service
-chmod 755 /data/onstart.sh
-chmod 755 /data/onstop.sh
+chmod 755 /onstart.sh
+chmod 755 /onstop.sh
 
 systemctl enable onstartstop --now
