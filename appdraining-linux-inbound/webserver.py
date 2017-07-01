d60c0a7216162c67fa3c59a2256d2505c618f63d
negat <negat@microsoft.com>
2017-07-01 05:05:14 +0000

    made web server log time as well

--- appdraining-linux-inbound/webserver.py
+++ appdraining-linux-inbound/webserver.py
@@ -1,10 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+import time
+
 @app.route("/")
 def hello():
     with open("/data/webserveroutput.txt", "a") as wo:
-        wo.write("incoming request!")
+        wo.write("incoming request at " + time.strftime("%c") + ".\n")
         
     return "Hello World!"
 
