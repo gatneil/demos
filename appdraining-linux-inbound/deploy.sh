8b71558d5489d14759420610e336779a61e6173e
negat <negat@microsoft.com>
2017-07-01 05:12:48 +0000

    simplified comments

--- appdraining-linux-inbound/deploy.sh
+++ appdraining-linux-inbound/deploy.sh
@@ -16,14 +16,16 @@ USERNAME=negat
 URLBASE="https://raw.githubusercontent.com/gatneil/demos/master/appdraining-linux-inbound"
 
 
-# create VM with onstart and onstop code (the onstop code will make sure we get the delay on delete);
+# create VM to be deleted; install onstart and onstop code;
+# the onstop code will make sure we get the delay on delete;
 # also install a web server that logs incoming requests to a data disk
 az group create -l ${LOCATION} -n ${RGNAME}
 PIP=$(az vm create --resource-group ${RGNAME} --name ${VMNAME} --image Canonical:UbuntuServer:16.04-LTS:latest --admin-username ${USERNAME} --ssh-key-value $PUBKEYPATH --nsg "" --vnet-name ${VNETNAME} | grep publicIpAddress | cut -d '"' -f 4)
 az vm disk attach --resource-group ${RGNAME} --vm-name ${VMNAME} --disk ${DISKNAME} --size-gb ${DISKSIZEGB} --new
 az vm extension set --publisher "Microsoft.Azure.Extensions" --name "CustomScript" --resource-group ${RGNAME} --vm-name ${VMNAME} --settings "{\"fileUris\": [\"${URLBASE}/install.sh\"], \"commandToExecute\": \"bash install.sh ${URLBASE}\"}"
 
-# create a VM to be deleted that will make connections to the other VM (these are inbound from the perspective of the VM getting deleted)
+# create a VM to make connections to the other VM;
+# these requests are inbound from the perspective of the VM getting deleted
 az vm create --resource-group ${RGNAME} --name ${VM2NAME} --image Canonical:UbuntuServer:16.04-LTS:latest --admin-username ${USERNAME} --ssh-key-value $PUBKEYPATH --nsg "" --vnet-name ${VNET2NAME}
 az vm extension set --publisher "Microsoft.Azure.Extensions" --name "CustomScript" --resource-group ${RGNAME} --vm-name ${VM2NAME} --settings "{\"fileUris\": [\"${URLBASE}/curlserver.sh\"], \"commandToExecute\": \"bash curlserver.sh ${PIP}:5000 &\"}"
 
