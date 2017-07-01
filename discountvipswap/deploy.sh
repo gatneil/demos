d53cf390d45fe11c9c95ecfb9a0e158ac86181dc
negat <negat@microsoft.com>
2017-07-01 03:40:06 +0000

    renamed to be more accurate

--- discountvipswap/deploy.sh
+++ discountvipswap/deploy.sh
@@ -8,17 +8,23 @@ rgname="nsglbswap$RANDOM"
 location="eastus2"
 vmssname="myvmss"
 vmss2name=${vmssname}2
+lbname="mylb"
+lb2name=${lbname}2
+vnetname="myvnet"
+subnetname="mysubnet"
 pubkeypath="/home/negat/.ssh/id_rsa.pub"
-uriBase="https://raw.githubusercontent.com/gatneil/demos/master/vmsslbswap"
+uriBase="https://raw.githubusercontent.com/gatneil/demos/master/discountvipswap"
 username="negat"
 
 az group create -l ${location} -n ${rgname}
 
-# create 2 scale sets: 1 with LB, 1 without
-az vmss create --resource-group $rgname --name $vmssname --image Canonical:UbuntuServer:16.04-LTS:latest --admin-username ${username} --ssh-key-value $pubkeypath --upgrade-policy-mode Automatic --instance-count 2
-az vmss create --resource-group $rgname --name $vmss2name --image Canonical:UbuntuServer:16.04-LTS:latest --admin-username ${username} --ssh-key-value $pubkeypath --upgrade-policy-mode Automatic --instance-count 2 --lb ""
+# create 2 scale sets, each with a load balancer; one with Static public IP
+az network lb create -g ${rgname} -n ${lbname} --vnet-name ${vnetname} --subnet ${subnetname} --public-ip-address-allocation Static
+az network lb create -g ${rgname} -n ${lb2name} --vnet-name ${vnetname} --subnet ${subnetname} --public-ip-address ""
+az vmss create --resource-group $rgname --name $vmssname --image Canonical:UbuntuServer:16.04-LTS:latest --admin-username ${username} --ssh-key-value $pubkeypath --upgrade-policy-mode Automatic --instance-count 2 --vnet-name ${vnetname} --subnet ${subnetname} --lb ${lbname}
+az vmss create --resource-group $rgname --name $vmss2name --image Canonical:UbuntuServer:16.04-LTS:latest --admin-username ${username} --ssh-key-value $pubkeypath --upgrade-policy-mode Automatic --instance-count 2 --vnet-name ${vnetname} --subnet ${subnetname} --lb ${lb2name}
 
-python swaplb.py --src_vmss_rg $rgname --src_vmss_name $vmssname --dest_vmss_rg $rgname --dest_vmss_name $vmss2name
+python discountvipswap.py --src_lb_rg $rgname --src_lb_name $lbname --dest_lb_rg $rgname --dest_lb_name $lb2name
 
 # get the name of the LB on the first scale set
 #lbname=$(az vmss show -g nsgvmss -n nsgvmss --output json | grep "/backendAddressPools" | cut -d "/" -f 9)
