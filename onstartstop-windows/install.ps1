90de5ec9c5330efac5babaae8b1d3a7cc21f9a5c
negat <negat@microsoft.com>
2017-08-10 22:54:19 +0000

    added initial part of gpt.ini

--- onstartstop-windows/install.ps1
+++ onstartstop-windows/install.ps1
@@ -4,3 +4,7 @@ Format-Volume -DriveLetter F -FileSystem NTFS -NewFileSystemLabel datadisk -Conf
 
 curl -UseBasicParsing https://raw.githubusercontent.com/gatneil/demos/master/onstartstop-windows/onstart.ps1 | select -Expand Content >> C:\onstart.ps1
 curl -UseBasicParsing https://raw.githubusercontent.com/gatneil/demos/master/onstartstop-windows/onstop.ps1 | select -Expand Content >> C:\onstop.ps1
+
+"[General]" >> C:\Windows\System32\GroupPolicy\gpt.ini
+"gPCMachineExtensionNames=[{42B5FAAE-6536-11D2-AE5A-0000F87571E3}{40B6664F-4972-11D1-A7CA-0000F87571E3}]" >> C:\Windows\System32\GroupPolicy\gpt.ini
+"Version=6" >> C:\Windows\System32\GroupPolicy\gpt.ini
