27f3757489e7d035b628309e2b149a120267fd07
negat <negat@microsoft.com>
2017-08-10 22:20:40 +0000

    made use basic parsing to avoid IE dependency

--- onstartstop-windows/install.ps1
+++ onstartstop-windows/install.ps1
@@ -2,5 +2,5 @@ Initialize-Disk -Number 2 -PartitionStyle MBR
 New-Partition -DiskNumber 2 -DriveLetter F -UseMaximumSize
 Format-Volume -DriveLetter F -FileSystem NTFS -NewFileSystemLabel datadisk -Confirm:$false
 
-curl https://raw.githubusercontent.com/gatneil/demos/master/onstartstop-windows/onstart.ps1 | select -Expand Content >> C:\onstart.ps1
-curl https://raw.githubusercontent.com/gatneil/demos/master/onstartstop-windows/onstop.ps1 | select -Expand Content >> C:\onstop.ps1
+curl -UseBasicParsing https://raw.githubusercontent.com/gatneil/demos/master/onstartstop-windows/onstart.ps1 | select -Expand Content >> C:\onstart.ps1
+curl -UseBasicParsing https://raw.githubusercontent.com/gatneil/demos/master/onstartstop-windows/onstop.ps1 | select -Expand Content >> C:\onstop.ps1
