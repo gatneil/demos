ac58bd8ae2b8a46435ce0d64c27369cf9e588bbe
negat <negat@microsoft.com>
2017-08-11 21:00:37 +0000

    added logic to install onstart and onstop scripts

--- onstartstop-windows/install.ps1
+++ onstartstop-windows/install.ps1
@@ -5,16 +5,17 @@ Format-Volume -DriveLetter F -FileSystem NTFS -NewFileSystemLabel datadisk -Conf
 curl -UseBasicParsing https://raw.githubusercontent.com/gatneil/demos/master/onstartstop-windows/onstart.ps1 | select -Expand Content >> C:\onstart.ps1
 curl -UseBasicParsing https://raw.githubusercontent.com/gatneil/demos/master/onstartstop-windows/onstop.ps1 | select -Expand Content >> C:\onstop.ps1
 
-#"[General]" >> C:\Windows\System32\GroupPolicy\gpt.ini
-#"gPCMachineExtensionNames=[{42B5FAAE-6536-11D2-AE5A-0000F87571E3}{40B6664F-4972-11D1-A7CA-0000F87571E3}]" >> C:\Windows\System32\GroupPolicy\gpt.ini
-#"Version=6" >> C:\Windows\System32\GroupPolicy\gpt.ini
+"[General]" >> C:\Windows\System32\GroupPolicy\gpt.ini
+"gPCMachineExtensionNames=[{42B5FAAE-6536-11D2-AE5A-0000F87571E3}{40B6664F-4972-11D1-A7CA-0000F87571E3}]" >> C:\Windows\System32\GroupPolicy\gpt.ini
+"Version=8" >> C:\Windows\System32\GroupPolicy\gpt.ini
 
+mkdir C:\Windows\System32\GroupPolicy\Machine\Scripts
 
-#mkdir C:\Windows\System32\GroupPolicy\Machine\Scripts
+"[Shutdown]" >> C:\Windows\System32\GroupPolicy\Machine\Scripts\psscripts.ini
+"0CmdLine=C:\onstop.ps1" >> C:\Windows\System32\GroupPolicy\Machine\Scripts\psscripts.ini
+"0Parameters=" >> C:\Windows\System32\GroupPolicy\Machine\Scripts\psscripts.ini
+"[Startup]" >> C:\Windows\System32\GroupPolicy\Machine\Scripts\psscripts.ini
+"0CmdLine=C:\onstart.ps1" >> C:\Windows\System32\GroupPolicy\Machine\Scripts\psscripts.ini
+"0Parameters=" >> C:\Windows\System32\GroupPolicy\Machine\Scripts\psscripts.ini
 
-#"[Startup]" >> C:\Windows\System32\GroupPolicy\Machine\Scripts\scripts.ini
-#"0CmdLine=C:\onstart.ps1" >> C:\Windows\System32\GroupPolicy\Machine\Scripts\scripts.ini
-#"0Parameters=" >> C:\Windows\System32\GroupPolicy\Machine\Scripts\scripts.ini
-#"[Shutdown]" >> C:\Windows\System32\GroupPolicy\Machine\Scripts\scripts.ini
-#"0CmdLine=C:\onstop.ps1" >> C:\Windows\System32\GroupPolicy\Machine\Scripts\scripts.ini
-#"0Parameters=" >> C:\Windows\System32\GroupPolicy\Machine\Scripts\scripts.ini
+gpupdate
