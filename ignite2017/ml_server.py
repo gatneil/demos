6422cd3e5d2d75801d795efcbb8ebccbba60400d
negat <negat@microsoft.com>
2017-09-16 01:19:17 +0000

    made ml server do something

--- ignite2017/ml_server.py
+++ ignite2017/ml_server.py
@@ -1,6 +1,18 @@
 from flask import Flask
 app = Flask(__name__)
 
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
+
 @app.route('/')
 def ml_server():
-        return 'ml server here!'
+        parser = PlaintextParser.from_string("Hi there! My name is Neil. Things are cool.", Tokenizer("english"))
+        summarizer = LexRankSummarizer()
+
+        summary = summarizer(parser.document, 2)
+        res = ""
+        for sentence in summary:
+                res += sentence
+            
+        return res
