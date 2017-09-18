d794f15168eefe678b598c247dfbd5b656a85f0f
negat <negat@microsoft.com>
2017-09-18 22:24:01 +0000

    initial commit of run script

--- ignite2017/run.sh
+++ ignite2017/run.sh
@@ -7,26 +7,23 @@ set -evx
 rgname="ignite$RANDOM"
 location="centralus"
 vmsku="Standard_F1"
-vmname="counter"
-vmssname="pushers"
-#pubkeypath="/home/negat/.ssh/id_rsa.pub"
+largevmsku="Standard_F16"
+vmname="client"
+vmssname="servers"
 uriBase="https://raw.githubusercontent.com/gatneil/scripts/master/"
 
 az group create --name $rgname --location $location
 
-az vmss create --resource-group $rgname --name $vmssname --image UbuntuLTS --instance-count 10
+az vmss create --resource-group $rgname --name $vmssname --image UbuntuLTS --instance-count 10 --app-gateway myAppGW --app-gateway-capacity 1 --backend-port 5000 --upgrade-policy-mode Automatic
 
-az vmss extension set --publisher "Microsoft.Azure.Extensions" --name "CustomScript" --resource-group $rgname --vmss-name $vmssname --settings "{\"fileUris\": [\"https://raw.githubusercontent.com/gatneil/demos/ignite2017/ignite2017/install_ml_server.sh\"], \"commandToExecute\": \"bash install_ml_server.sh ${pip}\"}"
+az vmss extension set --publisher "Microsoft.Azure.Extensions" --name "CustomScript" --resource-group $rgname --vmss-name $vmssname --settings "{\"fileUris\": [\"https://raw.githubusercontent.com/gatneil/demos/ignite2017/ignite2017/install_ml_server.sh\"], \"commandToExecute\": \"bash install_ml_server.sh\"}"
 
-exit 0
+vmsspip=`az network public-ip show --resource-group $rgname --name myAppGwPublicIP | grep  \"ipAddress\" | cut -d "\"" -f 4`
 
-az vm create --resource-group $rgname --name $vmname --image UbuntuLTS --admin-username negat --nsg "" --size $vmsku
+az vm create --resource-group $rgname --name $vmname --image UbuntuLTS --nsg "" --size $largevmsku
 
-az vm extension set --publisher "Microsoft.Azure.Extensions" --name "CustomScript" --resource-group $rgname --vm-name $vmname --settings "{\"fileUris\": [\"${uriBase}install_count_requests.sh\"], \"commandToExecute\": \"bash install_count_requests.sh ${uriBase}\"}"
+vmpip=`az network public-ip show --resource-group $rgname --name ${vmname}PublicIP | grep  \"ipAddress\" | cut -d "\"" -f 4`
 
-pip=`az network public-ip show --resource-group $rgname --name "$vmname"PublicIP | grep  \"ipAddress\" | cut -d "\"" -f 4`:5000
+az vm extension set --publisher "Microsoft.Azure.Extensions" --name "CustomScript" --resource-group $rgname --vm-name $vmname --settings "{\"fileUris\": [], \"commandToExecute\": \"while true; do curl ${vmsspip}/summarize -F text=\\\"Hi there! My name is Neil. Things are cool.\\\"; done &\"}"
 
-echo $pip
-
-
-
+ssh -o StrictHostKeyChecking=no ${vmpip} sudo tail -f /var/lib/waagent/custom-script/download/0/stdout
