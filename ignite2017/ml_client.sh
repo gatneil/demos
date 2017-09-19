3f6dbb4ad00d086c2f71187ff75da84770d44264
negat <negat@microsoft.com>
2017-09-19 02:07:58 +0000

    made client run forever

--- ignite2017/ml_client.sh
+++ ignite2017/ml_client.sh
@@ -4,7 +4,10 @@ set -evx
 
 curl https://raw.githubusercontent.com/gatneil/demos/ignite2017/ignite2017/emerson.txt -o emerson.txt
 
-while read -r line;
+while true;
 do
-    curl ${1}/summarize -F text="$line"
-done < emerson.txt
+    while read -r line;
+    do
+	curl ${1}/summarize -F text="$line"
+    done < emerson.txt
+done
