59d0e6410d40b14d93bf94d75c91b7e94be59944
negat <negat@microsoft.com>
2017-09-20 20:27:21 +0000

    making client a VMSS

--- ignite2017/run.sh
+++ ignite2017/run.sh
@@ -5,7 +5,7 @@
 set -evx
 
 rgname="ignite$RANDOM"
-location="centralus"
+location="southeastasia"
 vmsku="Standard_F1"
 largevmsku="Standard_F16"
 vmname="client"
@@ -14,16 +14,17 @@ uriBase="https://raw.githubusercontent.com/gatneil/scripts/master/"
 
 az group create --name $rgname --location $location
 
-az vmss create --resource-group $rgname --name $vmssname --image UbuntuLTS --instance-count 100 --app-gateway myAppGW --app-gateway-capacity 1 --backend-port 5000 --upgrade-policy-mode Automatic
+az vmss create --resource-group $rgname --name $vmssname --image UbuntuLTS --instance-count 1000 --app-gateway myAppGW --app-gateway-capacity 10 --backend-port 5000 --upgrade-policy-mode Automatic --vnet-address-prefix 10.0.0.0/20 --subnet-address-prefix 10.0.0.0/21
 
 az vmss extension set --publisher "Microsoft.Azure.Extensions" --name "CustomScript" --resource-group $rgname --vmss-name $vmssname --settings "{\"fileUris\": [\"https://raw.githubusercontent.com/gatneil/demos/ignite2017/ignite2017/install_ml_server.sh\"], \"commandToExecute\": \"bash install_ml_server.sh\"}"
 
 vmsspip=`az network public-ip show --resource-group $rgname --name myAppGwPublicIP | grep  \"ipAddress\" | cut -d "\"" -f 4`
 
-az vm create --resource-group $rgname --name $vmname --image UbuntuLTS --nsg "" --size $largevmsku
+#az vm create --resource-group $rgname --name $vmname --image UbuntuLTS --nsg "" --size $largevmsku
+#vmpip=`az network public-ip show --resource-group $rgname --name ${vmname}PublicIP | grep  \"ipAddress\" | cut -d "\"" -f 4`
+#az vm extension set --publisher "Microsoft.Azure.Extensions" --name "CustomScript" --resource-group $rgname --vm-name $vmname --settings "{\"fileUris\": [\"https://raw.githubusercontent.com/gatneil/demos/ignite2017/ignite2017/ml_client.sh\"], \"commandToExecute\": \"bash ml_client.sh ${vmsspip} &\"}"
+#ssh -o StrictHostKeyChecking=no ${vmpip} sudo tail -f /var/lib/waagent/custom-script/download/0/stdout
 
-vmpip=`az network public-ip show --resource-group $rgname --name ${vmname}PublicIP | grep  \"ipAddress\" | cut -d "\"" -f 4`
+az vmss create --resource-group $rgname --name $vmname --image UbuntuLTS --nsg "" --size $vmsku --instance-count 100
+az vmss extension set --publisher "Microsoft.Azure.Extensions" --name "CustomScript" --resource-group $rgname --vmss-name $vmname --settings "{\"fileUris\": [\"https://raw.githubusercontent.com/gatneil/demos/ignite2017/ignite2017/ml_client.sh\"], \"commandToExecute\": \"bash ml_client.sh ${vmsspip} &\"}"
 
-az vm extension set --publisher "Microsoft.Azure.Extensions" --name "CustomScript" --resource-group $rgname --vm-name $vmname --settings "{\"fileUris\": [\"https://raw.githubusercontent.com/gatneil/demos/ignite2017/ignite2017/ml_client.sh\"], \"commandToExecute\": \"bash ml_client.sh ${vmsspip} &; bash ml_client.sh ${vmsspip} &; bash ml_client.sh ${vmsspip} &; bash ml_client.sh ${vmsspip} &\"}"
-
-ssh -o StrictHostKeyChecking=no ${vmpip} sudo tail -f /var/lib/waagent/custom-script/download/0/stdout
