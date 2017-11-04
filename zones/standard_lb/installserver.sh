fa2213a17411bb757f5ff563875a668e4f90f064
Neil Sant Gat <negat@microsoft.com>
2017-11-04 03:10:44 +0000

    fix url

--- zones/standard_lb/installserver.sh
+++ zones/standard_lb/installserver.sh
@@ -7,7 +7,7 @@ apt-get install python-pip -y
 pip install --upgrade pip
 pip install flask
 
-curl -o server.py https://raw.githubusercontent.com/gatneil/demos/master/zones/standard_lb/master/server.py
+curl -o server.py https://raw.githubusercontent.com/gatneil/demos/master/zones/standard_lb/server.py
 
 export FLASK_APP=server.py
 
