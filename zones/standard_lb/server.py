a7abdcb3c05de1863958eef83ae969cd9d74e190
Neil Sant Gat <negat@microsoft.com>
2017-11-10 23:05:56 +0000

    intense

--- zones/standard_lb/server.py
+++ zones/standard_lb/server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def default_page():
     res = 0
-    for i in range(0, 100):
+    for i in range(0, 1000000):
         res += math.sqrt(i)
         
     return str(res)
